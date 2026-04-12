5760223a9e1d83431874d2e3366bbb415c9e467b
eclipse <ayoub.chouat@esprit.tn>
2026-04-12 15:53:09 +0100

    added the buttons with their variations

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,20 +1,20 @@
+import { type ButtonHTMLAttributes, type ReactNode } from "react";
+
 const variants = {
-  primary: "bg-orange-500 text-white",
-  dark: "bg-black text-white",
-  ghostTinted: "bg-yellow-200/40",
-  ghostPrimary: "bg-transparent hover:bg-gray-200",
+  primary:
+    "bg-(--primary) hover:bg-(--primary-light) text-(--text) px-3 py-2 rounded-md text-base font-semibold flex gap-2 items-center content-center cursor-pointer",
+  dark: "bg-(--secondary) px-3 py-2 text-(--bg) rounded-md font-semibold text-base hover:bg-(--secondary-light) cursor-pointer flex items-center contnent-center",
+  ghostTinted:
+    "bg-(--ghostTinted) px-3 py-2 rounded-md font-semibold text-base cursor-pointer flex items-center contnent-center hover:bg-(--ghostTinted-light)",
+  ghostPrimary:
+    "bg-(--ghostTinted) px-3 py-2 rounded-md font-semibold text-base cursor-pointer flex items-center contnent-center hover:bg-(--primary) border-(--bg-light) border",
 };
-
 type Variant = keyof typeof variants;
-
-function Button({
-  children,
-  variant = "primary",
-  ...props
-}: {
-  children: React.ReactNode;
-  variant?: Variant;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  variant: Variant;
+}
+function Button({ children, variant = "primary", ...props }: ButtonProps) {
   return (
     <button className={variants[variant]} {...props}>
       {children}
--- client/src/dev/ButtonsSandbox.tsx
+++ client/src/dev/ButtonsSandbox.tsx
@@ -1,6 +1,19 @@
+import Button from "../components/ui/Button";
+import { Sparkles, Plus } from "lucide-react";
 function ButtonsSendbox() {
-  return <>
-  
-  </>;
+  return (
+    <div className="flex gap-3">
+      <Button variant="primary">
+        <Sparkles className="w-5 h-5"></Sparkles>
+        Upgrade Plan
+      </Button>
+      <Button variant="dark">Sign Up</Button>
+      <Button variant="ghostTinted">Log in</Button>
+      <Button variant="ghostPrimary">Save</Button>
+      <Button variant="ghostTinted">
+        <Plus></Plus>
+      </Button>
+    </div>
+  );
 }
 export default ButtonsSendbox;
