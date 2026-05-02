9d6977be383b6e21566f49d29b4b8f83cd39d0d0
eclipse <ayoub.chouat@esprit.tn>
2026-05-02 15:58:54 +0100

    added the logo mascot

--- client/src/components/ui/Logo.tsx
+++ client/src/components/ui/Logo.tsx
@@ -1,7 +1,8 @@
+import logo from "../../assets/pictures/logo.svg";
 function Logo() {
   return (
-    <div className="flex items-center gap-2">
-      {/* <img src={logo} className="w-10" alt="Atman Logo Image" /> */}
+    <div className="flex items-end gap-3">
+      <img src={logo} className="w-7"/>
       <h1 className=" text-(--text) text-2xl font-serif font-bold">Atman</h1>
     </div>
   );
