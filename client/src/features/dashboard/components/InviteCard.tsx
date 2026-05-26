b33ff982beff4007d1ae225aeaa9e68e41375355
ayoubchwt <ayoub.chouat@esprit.tn>
2026-05-26 23:02:42 +0100

    styled and polished the invite status box

--- client/src/features/dashboard/components/InviteCard.tsx
+++ client/src/features/dashboard/components/InviteCard.tsx
@@ -31,27 +31,29 @@ function InviteCard({
     },
   ];
   return (
-    <div className="flex items-center">
-      <ShareUserIcon letter={letter} className="w-8 h-8"></ShareUserIcon>
-      <div className="flex flex-col">
-        <div className="flex items-center">
-          <h1>{name}</h1>
-          <div className="">
-            <Clock className="w-3 h-3" />
-            <p className="">{status}</p>
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <ShareUserIcon letter={letter} className="w-8 h-8"></ShareUserIcon>
+        <div className="flex flex-col">
+          <div className="flex items-center gap-3">
+            <h1 className="text-(--text) text-sm">{name}</h1>
+            <div className="flex items-center gap-1 bg-(--bg-dark) py-1 px-2 rounded-xl">
+              <Clock className="text-(--text-light) w-3 h-3" />
+              <p className="text-xs text-(--text-light)">{status}</p>
+            </div>
           </div>
+          <p className="text-xs text-(--text-light)">{email}</p>
         </div>
-        <p>{email}</p>
       </div>
-      <div>
+      <div className="flex items-center gap-2">
         <DropDown
           options={options}
           setValue={() => onChange}
           value={role}
-          className="w-full"
+          className="w-26 h-8"
         ></DropDown>
-        <Button variant="ghost" onClick={onDelete}>
-          <Trash />
+        <Button variant="ghostTinted" onClick={onDelete} className="p-2 rounded-xl">
+          <Trash className="text-(--text-light) w-4 h-4"/>
         </Button>
       </div>
     </div>
